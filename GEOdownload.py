dacf0d7f282c68a8b35f67b486b222cebe56c307
jslit <jslit@jungsoo080ui-MacBookPro.local>
2023-01-05 17:41:01 +0900

    refactor

--- GEOdownload.py
+++ GEOdownload.py
@@ -1,7 +1,8 @@
 """
 Author: Jungsoo Lee
 Date: January 5, 2023,
-Description: This script automates downloading supplementary files from GEO.
+
+This script automates downloading supplementary files from GEO.
 """
 import os
 import wget
@@ -21,6 +22,7 @@ class NewDir(ProcessDir):
         self.get_dir = pre_path + new_dir
 
     def process(self):
+        """Create a new directory."""
         try:
             os.makedirs(self.get_dir, exist_ok=True)
         except OSError as e:
@@ -37,23 +39,28 @@ class DownloadWebfile(ABC):
     def run_download(self, data_name):
         pass
 
-    def destination_dir(self, destination_dir):
-        self.destination_dir = destination_dir
-
 
 class DownloadGEO(DownloadWebfile):
     def run_download(self, data_name):
+        """Download supplementary files from GEO."""
+        # Create a new directory for the supplementary files
         new_dir = NewDir(self.destination_dir, data_name)
         new_dir.process()
+
+        # Retrieve metadata for the given GEO accession number
         gse = get_GEO(geo=data_name, destdir=self.destination_dir)
+
+        # Download the supplementary files
         for supp_files in gse.metadata['supplementary_file']:
             try:
+                # Save the file to the new directory created above
                 wget.download(supp_files, out=new_dir.get_dir)
             except Exception as e:
-                print(f'An error occurred: {e}')
+                print(f'An error occurred while downloading the file: {e}')
 
 
 if __name__ == "__main__":
+    """Download supplementary files from GEO."""
     supp_downloader = DownloadGEO("/Users/jslit/Documents/")
     supp_downloader.run_download("GSE185553")
     supp_downloader.run_download("GSE185277")
--- test_GEOdownload.py
+++ test_GEOdownload.py
@@ -1,5 +1,6 @@
 from GEOdownload import *
 
 
+"""Download supplementary files from GEO."""
 supp_downloader = DownloadGEO("/Users/jslit/Desktop/")
 supp_downloader.run_download("GSE87581")
